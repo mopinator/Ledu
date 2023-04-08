8cb48bf78a9388d97c1fae187e809bad04b7c754
Simon Li <li.simon021@gmail.com>
2023-04-09 03:57:20 +1200

    added loading screen and updated css

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,40 @@ import Contact from "./pages/Contact";
 import Gallery from "./pages/Gallery";
 import Menu from "./pages/Menu";
 import Team from "./pages/Team";
+import { useState, useEffect } from "react";
+import PacmanLoader from "react-spinners/PacmanLoader";
 
 function App() {
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		setLoading(true);
+		setTimeout(() => {
+			setLoading(false);
+		}, 2500);
+	}, []);
+
 	return (
-		<div className={styles.App}>
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<NavLayout />}>
-						<Route index element={<Home />} />
-						<Route path="about" element={<About />} />
-						<Route path="awards" element={<Awards />} />
-						<Route path="contact" element={<Contact />} />
-						<Route path="gallery" element={<Gallery />} />
-						<Route path="menu" element={<Menu />} />
-						<Route path="team" element={<Team />} />
-					</Route>
-				</Routes>
-			</BrowserRouter>
+		<div className={styles[`App`]}>
+			{loading ? (
+				<div className={styles[`loader-container`]}>
+					<PacmanLoader />
+				</div>
+			) : (
+				<BrowserRouter>
+					<Routes>
+						<Route path="/" element={<NavLayout />}>
+							<Route index element={<Home />} />
+							<Route path="about" element={<About />} />
+							<Route path="awards" element={<Awards />} />
+							<Route path="contact" element={<Contact />} />
+							<Route path="gallery" element={<Gallery />} />
+							<Route path="menu" element={<Menu />} />
+							<Route path="team" element={<Team />} />
+						</Route>
+					</Routes>
+				</BrowserRouter>
+			)}
 		</div>
 	);
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,17 @@
 import Logo from "./images/ledu.jpg";
 import styles from "../Home/Home.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebook, faInstagram, faInstagramSquare } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope, faLocationPin } from "@fortawesome/free-solid-svg-icons";
+import { faFacebook, faInstagramSquare } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope, faLocationDot } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
 	return (
-		<>
+		<div className={styles[`main-container`]}>
 			<div className={styles[`img-container`]}>
 				<img src={Logo} alt="ledu-logo" />
 			</div>
-
 			<div className={styles[`sub-text`]}>ฤดู - Seasonal</div>
-			<div className={styles.container}>
+			<div className={styles[`container`]}>
 				<div className={styles[`social-media`]}>
 					<a href="https://www.facebook.com/LeDuWineBarandrestaurant/" target="_blank">
 						<FontAwesomeIcon icon={faFacebook} />
@@ -24,10 +23,10 @@ export default function Home() {
 						<FontAwesomeIcon icon={faEnvelope} />
 					</a>
 					<a href="https://goo.gl/maps/XJcEeLyfYbk1eFXi6" target="_blank">
-						<FontAwesomeIcon icon={faLocationPin} />
+						<FontAwesomeIcon icon={faLocationDot} />
 					</a>
 				</div>
 			</div>
-		</>
+		</div>
 	);
 }
