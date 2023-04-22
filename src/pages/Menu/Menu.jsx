aadf092b4c6b806e4340b8f7b9216e8298d4bbe7
Simon Li <li.simon021@gmail.com>
2023-04-23 00:00:04 +1200

    changed menu page to include images and updated layout. Media queries added for majority of popular screensizes

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,6 +1,8 @@
 import styles from "./Menu.module.css";
-import { menuData } from "./data";
-
+import pic1 from "./images/pic01.png";
+import pic2 from "./images/pic02.png";
+// import pic3 from "./images/pic03.jpg";
+// import pic4 from "./images/pic04.jpg";
 export default function Menu() {
 	return (
 		<>
@@ -8,68 +10,56 @@ export default function Menu() {
 				<div className={styles["inner-container"]}>
 					<div className={styles["four-course-container"]}>
 						<div className={styles["four-course-menu"]}>
-							<h3 className={styles["sub-heading"]}>4 Course</h3>
-							<p>Amuse-Bouche</p>
+							<div className={styles["menu-inner-container-top"]}>
+								<h3 className={styles["sub-heading"]}>4 Course</h3>
+								<p>Amuse-Bouche</p>
 
-							<div className={styles["menu-option-container-one"]}>
-								<p className={styles["menu-option"]}>Banana Prawn | Seaweed | Toasted Chili</p>
-								<p className={styles["or"]}>or</p>
-								<p className={styles["menu-option"]}>Khao Chae</p>
+								<div className={styles["menu-option-container-one"]}>
+									<p className={styles["menu-option"]}>Banana Prawn | Seaweed | Toasted Chili</p>
+									<p className={styles["or"]}>or</p>
+									<p className={styles["menu-option"]}>Khao Chae</p>
+								</div>
+
+								<div className={styles["menu-option-container-two"]}>
+									<p className={styles["menu-option"]}>Golden Snapper | Ant Larvae | Sticky Rice</p>
+									<p className={styles["or"]}>or</p>
+									<p className={styles["menu-option"]}>Soft-Shell Crab | Southern Rice & Curry</p>
+								</div>
+								<p>Thai Wagyu | Northern Pepper | Khao-Soi</p>
+								<p>Fermented Rice | Marian Plum | Lemon</p>
+								<p>Petit Four</p>
+								<p>4 Course Menu - 3,490++</p>
+								<p>4 Glass Wine Paring - 1,500++</p>
+								<p>4 Glass Kombucha Pairing - 800++</p>
+								<p>Signature River Prawn - 1,200++</p>
+								<footer className={styles["footer"]}>-All prices are subjected to 10% service charge & 7% VAT-</footer>
 							</div>
-
-							<div className={styles["menu-option-container-two"]}>
-								<p className={styles["menu-option"]}>Golden Snapper | Ant Larvae | Sticky Rice</p>
-								<p className={styles["or"]}>or</p>
-								<p className={styles["menu-option"]}>Soft-Shell Crab | Southern Rice & Curry</p>
-							</div>
-
-							<p>Thai Wagyu | Northern Pepper | Khao-Soi</p>
-
-							<p>Fermented Rice | Marian Plum | Lemon</p>
-
-							<p>Petit Four</p>
 						</div>
-						<div className={styles["four-course-price"]}>
-							<p>4 Course Menu - 3,490++</p>
-
-							<p>4 Glass Wine Paring - 1,500++</p>
-
-							<p>4 Glass Kombucha Pairing - 800++</p>
-
-							<p>Signature River Prawn - 1,200++</p>
-
-							<p className={styles["footer"]}>-All prices are subjected to 10% service charge & 7% VAT-</p>
+						<div className={styles["top-image-container"]}>
+							<img className={styles["top-image"]} src={pic1} />
 						</div>
 					</div>
 					<div className={styles["six-course-container"]}>
 						<div className={styles["six-course-menu"]}>
-							<h3 className={styles["sub-heading"]}>6 Course</h3>
-							<p>Amuse-Bouche</p>
-
-							<p>Banana Prawn | Seaweed | Toasted Chili</p>
-
-							<p>Khao Chae</p>
-
-							<p>Golden Snapper | Ant Larvae | Sticky Rice</p>
-
-							<p>Soft-Shell Crab | Southern Rice & Curry</p>
-
-							<p>Thai Wagyu | Northern Pepper | Khao-Soi</p>
-
-							<p>Fermented Rice | Marian Plum | Lemon</p>
-
-							<p>Petit Four</p>
+							<div className={styles["menu-inner-container-bot"]}>
+								<h3 className={styles["sub-heading"]}>6 Course</h3>
+								<p>Amuse-Bouche</p>
+								<p>Banana Prawn | Seaweed | Toasted Chili</p>
+								<p>Khao Chae</p>
+								<p>Golden Snapper | Ant Larvae | Sticky Rice</p>
+								<p>Soft-Shell Crab | Southern Rice & Curry</p>
+								<p>Thai Wagyu | Northern Pepper | Khao-Soi</p>
+								<p>Fermented Rice | Marian Plum | Lemon</p>
+								<p>Petit Four</p>
+								<p>6 Course Menu - 3,990++</p>
+								<p>6 Glass Wine Paring - 2,000++</p>
+								<p>6 Glass Kombucha Pairing - 100++</p>
+								<p>Signature River Prawn - 1,200++</p>
+								<footer className={styles["footer"]}>-All prices are subjected to 10% service charge & 7% VAT-</footer>
+							</div>
 						</div>
-						<div className={styles["six-course-price"]}>
-							<p>6 Course Menu - 3,990++</p>
-
-							<p>6 Glass Wine Paring - 2,000++</p>
-
-							<p>6 Glass Kombucha Pairing - 100++</p>
-
-							<p>Signature River Prawn - 1,200++</p>
-
-							<p className={styles["footer"]}>-All prices are subjected to 10% service charge & 7% VAT-</p>
+						<div className={styles["bot-image-container"]}>
+							<img className={styles["bot-image"]} src={pic2} />
 						</div>
 					</div>
 				</div>
